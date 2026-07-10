1d197dd0f76e95737ef298034486fc7f47bb808b
laboratorlakol <lspd858@gmail.com>
2026-07-10 19:22:31 +0300

    ja
    ja

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,17 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.imgur.com" },
+      { protocol: "https", hostname: "imgur.com" },
+      { protocol: "https", hostname: "cdn.discordapp.com" },
+      { protocol: "https", hostname: "media.discordapp.net" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "**.amazonaws.com" },
+    ],
+  },
+};
+
 export default nextConfig;
